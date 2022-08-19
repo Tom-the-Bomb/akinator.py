db13b3047c7df336c3fc96171e552ad6db3337f3
Tom-the-Bomb <thello522@gmail.com>
2022-08-19 13:29:05 -0400

    added asyncakinator

---
+++ src/async_akinator.rs
@@ -0,0 +1,266 @@
+use crate::{
+    enums::{
+        Theme,
+        Answer,
+        Language,
+    },
+    error::Error,
+    models::Guess,
+};
+use tokio::sync::RwLock;
+use std::sync::Arc;
+
+use akinator_rs::Akinator as AkinatorStruct;
+use pyo3_asyncio::tokio::future_into_py as to_coro;
+use pyo3::prelude::*;
+
+/// Represents an async akinator game
+///
+/// .. note ::
+///     All attributes and methods are the same as the blocking `Akinator` class
+///     but instead all methods should be awaited
+///
+/// Parameters
+/// ----------
+/// theme : Optional[:class:`Theme`]
+///     the theme of the akinator game, would be one of `Characters`, `Animals` or `Objects`
+///     pass in using an answer enum, using the `from_str` classmethod if necessary, defaults to `Characters`
+/// language : Optional[:class:`Language`]
+///     the language for the akinator game, refer to the `Language` enum
+/// child_mode : Optional[bool]
+///     when set to `True`, NSFW content will not be provided
+///
+/// The parameters are also set as properties which also have a setter to change the values if necessary in the future
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct AsyncAkinator(
+    Arc<RwLock<AkinatorStruct>>,
+);
+
+#[pymethods]
+impl AsyncAkinator {
+    #[new]
+    #[args("*", theme, language, child_mode)]
+    fn constructor(
+        theme: Option<Theme>,
+        language: Option<Language>,
+        child_mode: Option<bool>,
+    ) -> Self {
+        let mut akinator =
+            AkinatorStruct::new();
+
+        if let Some(theme) = theme {
+            akinator = akinator.with_theme(theme.into());
+        }
+
+        if let Some(language) = language {
+            akinator = akinator.with_language(language.into());
+        }
+
+        if child_mode.unwrap_or(false) {
+            akinator = akinator.with_child_mode();
+        }
+
+        Self(
+            Arc::new(RwLock::new(akinator))
+        )
+    }
+
+    /// *coroutine*
+    ///
+    /// Starts the akinator game
+    /// and returns the first question
+    ///
+    /// Returns
+    /// -------
+    /// Optional[str]
+    fn start_game<'a>(&'a mut self, py: Python<'a>) -> PyResult<&'a pyo3::PyAny> {
+        let cloned = self.0.clone();
+
+        to_coro(py,
+            async move {
+                let mut writer = cloned.write()
+                    .await;
+
+                writer.start().await
+                    .map_err(|e| Error::from(e).into())
+            }
+        )
+    }
+
+    /// *coroutine*
+    ///
+    /// Answers the akinator's current question
+    /// with the provided `answer`
+    /// and returns the next question
+    ///
+    /// Returns
+    /// -------
+    /// Optional[str]
+    fn answer<'a>(&'a mut self, py: Python<'a>, answer: Answer) -> PyResult<&'a pyo3::PyAny> {
+        let cloned = self.0.clone();
+
+        to_coro(py,
+            async move {
+                let mut writer = cloned.write()
+                    .await;
+
+                writer.answer(answer.into()).await
+                    .map_err(|e| Error::from(e).into())
+            }
+        )
+    }
+
+    /// *coroutine*
+    ///
+    /// Tells the akinator to end the game and make its guess
+    /// should be called once when the `progression` is high enough such as `>=80.0`
+    /// and returns its best guess
+    ///
+    /// Returns
+    /// -------
+    /// Optional[:class:`Guess`]
+    fn win<'a>(&'a mut self, py: Python<'a>) -> PyResult<&'a pyo3::PyAny> {
+        let cloned = self.0.clone();
+
+        to_coro(py,
+            async move {
+                let mut writer = cloned.write()
+                    .await;
+
+                writer.win().await
+                    .map(|result| {
+                        result.map(Guess)
+                    })
+                    .map_err(|e| Error::from(e).into())
+            }
+        )
+    }
+
+    /// *coroutine*
+    ///
+    /// Goes back a question
+    /// and returns said (current) question
+    ///
+    /// Returns
+    /// -------
+    /// Optional[str]
+    fn back<'a>(&'a mut self, py: Python<'a>) -> PyResult<&'a pyo3::PyAny> {
+        let cloned = self.0.clone();
+
+        to_coro(py,
+            async move {
+                let mut writer = cloned.write()
+                    .await;
+
+                writer.back().await
+                    .map_err(|e| Error::from(e).into())
+            }
+        )
+    }
+
+    /// :class:`Theme`: the theme of the akinator game
+    #[getter]
+    fn theme(&self) -> Theme {
+        let reader = self.0
+            .blocking_read();
+
+        reader.theme.into()
+    }
+
+    /// :class:`Language`: the language of the akinator game
+    #[getter]
+    fn language(&self) -> Language {
+        let reader = self.0
+            .blocking_read();
+
+        reader.language.into()
+    }
+
+    /// bool: whether `child_mode` is on or off for the akinator game
+    #[getter]
+    fn child_mode(&self) -> bool {
+        let reader = self.0
+            .blocking_read();
+
+        reader.child_mode
+    }
+
+    /// Optional[str]: the current question of the akinator game
+    #[getter]
+    fn question(&self) -> Option<String> {
+        let reader = self.0
+            .blocking_read();
+
+        reader.current_question.clone()
+    }
+
+    /// float: the progression of the akinator
+    #[getter]
+    fn progression(&self) -> f32 {
+        let reader = self.0
+            .blocking_read();
+
+        reader.progression
+    }
+
+    /// int: a counter for the question # the akinator is on currently
+    #[getter]
+    fn step(&self) -> usize {
+        let reader = self.0
+            .blocking_read();
+
+        reader.step
+    }
+
+    /// Optional[:class:`Guess`]: the akinator's best guess
+    #[getter]
+    fn first_guess(&self) -> Option<Guess> {
+        let reader = self.0
+            .blocking_read();
+
+        reader.first_guess
+            .clone()
+            .map(Guess)
+    }
+
+    /// List[:class:`Guess`]: a list of all the akinator's potential guesses, ordered
+    #[getter]
+    fn guesses(&self) -> Vec<Guess> {
+        let reader = self.0
+            .blocking_read();
+
+        reader.guesses
+            .clone()
+            .into_iter()
+            .map(Guess)
+            .collect()
+    }
+
+    /// property setter to set `self.theme`
+    #[setter]
+    fn set_theme(&mut self, theme: Theme) {
+        let mut writer = self.0
+            .blocking_write();
+
+        writer.theme = theme.into();
+    }
+
+    /// property setter to set `self.language`
+    #[setter]
+    fn set_language(&mut self, language: Language) {
+        let mut writer = self.0
+            .blocking_write();
+
+        writer.language = language.into();
+    }
+
+    /// property setter to set `self.child_mode`
+    #[setter]
+    fn set_child_mode(&mut self, child_mode: bool) {
+        let mut writer = self.0
+            .blocking_write();
+
+        writer.child_mode = child_mode;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,219 +4,23 @@ use crate::{
         Answer,
         Language,
     },
-    error::Error,
+    async_akinator::AsyncAkinator,
+    blocking_akinator::Akinator,
     models::Guess,
 };
 
-use akinator_rs::Akinator as AkinatorStruct;
-use lazy_static::lazy_static;
-use tokio::runtime::Runtime;
 use pyo3::prelude::*;
 
+pub mod blocking_akinator;
+pub mod async_akinator;
 pub mod enums;
 pub mod error;
 pub mod models;
 
-lazy_static! {
-    static ref RUNTIME: Runtime = Runtime::new().unwrap();
-}
-
-
-/// Represents an akinator game
-///
-/// Parameters
-/// ----------
-/// theme : Optional[:class:`Theme`]
-///     the theme of the akinator game, would be one of `Characters`, `Animals` or `Objects`
-///     pass in using an answer enum, using the `from_str` classmethod if necessary, defaults to `Characters`
-/// language : Optional[:class:`Language`]
-///     the language for the akinator game, refer to the `Language` enum
-/// child_mode : Optional[bool]
-///     when set to `True`, NSFW content will not be provided
-///
-/// The parameters are also set as properties which also have a setter to change the values if necessary in the future
-#[pyclass]
-#[derive(Debug, Clone)]
-pub struct Akinator(
-    AkinatorStruct,
-);
-
-#[pymethods]
-impl Akinator {
-    #[new]
-    #[args("*", theme, language, child_mode)]
-    fn constructor(
-        theme: Option<Theme>,
-        language: Option<Language>,
-        child_mode: Option<bool>,
-    ) -> Self {
-        let mut akinator =
-            AkinatorStruct::new();
-
-        if let Some(theme) = theme {
-            akinator = akinator.with_theme(theme.into());
-        }
-
-        if let Some(language) = language {
-            akinator = akinator.with_language(language.into());
-        }
-
-        if child_mode.unwrap_or(false) {
-            akinator = akinator.with_child_mode();
-        }
-
-        Self(akinator)
-    }
-
-    fn __repr__(&self) -> String {
-        format!(
-            "<Akinator theme=\"{:?}\" language=\"{:?}\" child_mode={}>",
-            self.theme(),
-            self.language(),
-            self.child_mode(),
-        )
-    }
-
-    /// Starts the akinator game
-    /// and returns the first question
-    ///
-    /// Returns
-    /// -------
-    /// Optional[str]
-    fn start_game<'a>(&'a mut self, _py: Python<'a>) -> PyResult<Option<String>> {
-        RUNTIME.block_on(
-            async move {
-                self.0.start().await
-                    .map_err(|e| Error::from(e).into())
-            }
-        )
-    }
-
-    /// Answers the akinator's current question
-    /// with the provided `answer`
-    /// and returns the next question
-    ///
-    /// Returns
-    /// -------
-    /// Optional[str]
-    fn answer<'a>(&'a mut self, _py: Python<'a>, answer: Answer) -> PyResult<Option<String>> {
-        RUNTIME.block_on(
-            async move {
-                self.0.answer(answer.into()).await
-                    .map_err(|e| Error::from(e).into())
-            }
-        )
-    }
-
-    /// Tells the akinator to end the game and make its guess
-    /// should be called once when the `progression` is high enough such as `>=80.0`
-    /// and returns its best guess
-    ///
-    /// Returns
-    /// -------
-    /// Optional[:class:`Guess`]
-    fn win<'a>(&'a mut self, _py: Python<'a>) -> PyResult<Option<Guess>> {
-        RUNTIME.block_on(
-            async move {
-                self.0.win().await
-                    .map(|result| {
-                        result.map(Guess)
-                    })
-                    .map_err(|e| Error::from(e).into())
-            }
-        )
-    }
-
-    /// Goes back a question
-    /// and returns said (current) question
-    ///
-    /// Returns
-    /// -------
-    /// Optional[str]
-    fn back<'a>(&'a mut self, _py: Python<'a>) -> PyResult<Option<String>> {
-        RUNTIME.block_on(
-            async move {
-                self.0.back().await
-                    .map_err(|e| Error::from(e).into())
-            }
-        )
-    }
-
-    /// :class:`Theme`: the theme of the akinator game
-    #[getter]
-    fn theme(&self) -> Theme {
-        self.0.theme.into()
-    }
-
-    /// :class:`Language`: the language of the akinator game
-    #[getter]
-    fn language(&self) -> Language {
-        self.0.language.into()
-    }
-
-    /// bool: whether `child_mode` is on or off for the akinator game
-    #[getter]
-    const fn child_mode(&self) -> bool {
-        self.0.child_mode
-    }
-
-    /// Optional[str]: the current question of the akinator game
-    #[getter]
-    fn question(&self) -> Option<String> {
-        self.0.current_question.clone()
-    }
-
-    /// float: the progression of the akinator
-    #[getter]
-    const fn progression(&self) -> f32 {
-        self.0.progression
-    }
-
-    /// int: a counter for the question # the akinator is on currently
-    #[getter]
-    const fn step(&self) -> usize {
-        self.0.step
-    }
-
-    /// Optional[:class:`Guess`]: the akinator's best guess
-    #[getter]
-    fn first_guess(&self) -> Option<Guess> {
-        self.0.first_guess
-            .clone()
-            .map(Guess)
-    }
-
-    /// List[:class:`Guess`]: a list of all the akinator's potential guesses, ordered
-    #[getter]
-    fn guesses(&self) -> Vec<Guess> {
-        self.0.guesses
-            .clone()
-            .into_iter()
-            .map(Guess)
-            .collect()
-    }
-
-    /// property setter to set `self.theme`
-    #[setter]
-    fn set_theme(&mut self, theme: Theme) {
-        self.0.theme = theme.into();
-    }
-
-    /// property setter to set `self.language`
-    #[setter]
-    fn set_language(&mut self, language: Language) {
-        self.0.language = language.into();
-    }
-
-    /// property setter to set `self.child_mode`
-    #[setter]
-    fn set_child_mode(&mut self, child_mode: bool) {
-        self.0.child_mode = child_mode;
-    }
-}
 
 #[pymodule]
 fn akinator(py: Python<'_>, module: &PyModule) -> PyResult<()> {
+    module.add_class::<AsyncAkinator>()?;
     module.add_class::<Akinator>()?;
     module.add_class::<Guess>()?;
 
