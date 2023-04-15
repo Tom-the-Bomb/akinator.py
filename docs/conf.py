1d26d78b402beb67a35f206ea858015c8a5458df
Tom-the-Bomb <thello522@gmail.com>
2023-04-15 17:15:00 -0400

    fixed lamguage issue; rebuild

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Tom-the-Bomb'
 author = 'Tom-the-Bomb'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.2.3'
 
 
 # -- General configuration ---------------------------------------------------
